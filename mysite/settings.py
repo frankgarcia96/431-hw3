a1b3fa591145e049634a3611ec40e02300e7400b
frankgarcia96 <fgh1996@hotmail.com>
2019-02-13 18:57:41 -0600

    trying to deploy..

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,5 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+import dj_databse_url
+db_from_env = dj_databse_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_URL = '/static'
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
